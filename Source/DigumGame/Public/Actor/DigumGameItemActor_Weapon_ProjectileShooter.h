a8b572661d3ee133affe83817a4119401f11be23
cacayperez <cacayperez@outlook.com>
2024-05-30 20:32:07 +0800

    organized assets

---
+++ Source/DigumGame/Public/Actor/DigumGameItemActor_Weapon_ProjectileShooter.h
@@ -0,0 +1,18 @@
+﻿// Copyright Side C Studios Corporation, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DigumGameItemActor_ActiveItem.h"
+#include "UObject/Object.h"
+#include "DigumGameItemActor_Weapon_ProjectileShooter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIGUMGAME_API ADigumGameItemActor_Weapon_ProjectileShooter : public ADigumGameItemActor_ActiveItem
+{
+	GENERATED_BODY()
+public:
+};
